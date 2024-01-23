ec908392ff4daa0d7cff4c519226a326eba18ba9
Iván Sevilla Gómez <ivansevillagomez@protonmail.com>
2024-01-23 18:01:37 +0100

    Reflex init

---
+++ portafolio/portafolio.py
@@ -0,0 +1,16 @@
+import reflex as rx
+
+def index() -> rx.component:
+    return rx.box(
+
+    )
+
+app = rx.App(
+
+)
+app.add_page(
+    index,
+    title="Iván Sevilla, software developer",
+    description = "Prueba"
+)
+app.compile()
